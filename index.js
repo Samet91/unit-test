d3a9a970dfb7e3332b0c1e75a4cae5bcd71edccb
Samet Uslu <samet_u@hotmail.de>
2021-10-18 11:58:39 +0200

    Add node_modules, gitignore and new files

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function getGreeting(name = 'you') {
-  return `Hello ${name}!`
-}
+import { getGreeting } from './getGreeting.js'
+import { replaceName } from './replaceName.js'
+
 console.log(getGreeting('Jane Doe'))
 
-function replaceName(text, name, newName) {
-  return text.replaceAll(name, newName)
-}
 console.log(replaceName('Hello Jane. goodbye Jane', 'Jane', 'John'))
-
