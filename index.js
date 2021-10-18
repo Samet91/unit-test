8660e3d03a9306a2c5b8dd77297be552d1e67a3e
Samet Uslu <samet_u@hotmail.de>
2021-10-18 11:42:12 +0200

    Add replaceName

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-
 function getGreeting(name = 'you') {
-  return 'Hello ' + name + '!'
+  return `Hello ${name}!`
 }
-console.log()
+console.log(getGreeting('Jane Doe'))
+
+function replaceName(text, name, newName) {
+  return text.replaceAll(name, newName)
+}
+console.log(replaceName('Hello Jane. goodbye Jane', 'Jane', 'John'))
+
